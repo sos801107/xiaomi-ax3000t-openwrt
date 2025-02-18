dcf730ba7127aec9130be25f3b8d56dd7822c234
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-02-18 12:13:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,50 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 将packages源的相关文件替换成passwall_packages源的
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/dns2socks
+rm -rf feeds/packages/net/dns2tcp
+rm -rf feeds/packages/net/microsocks
+cp -r feeds/passwall_packages/xray-core feeds/packages/net
+cp -r feeds/passwall_packages/v2ray-geodata feeds/packages/net
+cp -r feeds/passwall_packages/sing-box feeds/packages/net
+cp -r feeds/passwall_packages/chinadns-ng feeds/packages/net
+cp -r feeds/passwall_packages/dns2socks feeds/packages/net
+cp -r feeds/passwall_packages/dns2tcp feeds/packages/net
+cp -r feeds/passwall_packages/microsocks feeds/packages/net
+
+# 修改golang源码以编译xray1.8.8+版本
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
+
+# 修改frp版本为官网最新v0.61.1 https://github.com/fatedier/frp 格式：https://codeload.github.com/fatedier/frp/tar.gz/v${PKG_VERSION}?
+rm -rf feeds/packages/net/frp
+wget https://github.com/coolsnowwolf/packages/archive/0f7be9fc93d68986c179829d8199824d3183eb60.zip -O OldPackages.zip
+unzip OldPackages.zip
+cp -r packages-0f7be9fc93d68986c179829d8199824d3183eb60/net/frp feeds/packages/net/
+rm -rf OldPackages.zip packages-0f7be9fc93d68986c179829d8199824d3183eb60s
+sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.61.1/' feeds/packages/net/frp/Makefile
+sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=95c567188d5635a7ac8897a6f93ae0568d0ac4892581a96c89874a992dd6a73c/' feeds/packages/net/frp/Makefile
+
+# 添加aliyundrive-webdav
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+git clone https://github.com/messense/aliyundrive-webdav.git
+cp -r aliyundrive-webdav/openwrt/aliyundrive-webdav feeds/packages/multimedia
+cp -r aliyundrive-webdav/openwrt/luci-app-aliyundrive-webdav feeds/luci/applications
+rm -rf aliyundrive-webdav
+
+# 跟随最新版naiveproxy
+rm -rf feeds/passwall_packages/naiveproxy
+rm -rf feeds/helloworld/naiveproxy
+git clone -b v5 https://github.com/sbwml/openwrt_helloworld.git
+cp -r openwrt_helloworld/naiveproxy feeds/passwall_packages
+cp -r openwrt_helloworld/naiveproxy feeds/helloworld
+rm -rf openwrt_helloworld
